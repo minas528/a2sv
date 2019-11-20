79c449909c55e9e52f2b07f44c9cd661e3e1ba33
minas528 <54796851+minas528@users.noreply.github.com>
2019-11-20 23:52:51 +0300

    Patterns Drawer

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void DrawRightAngledTriangle(int size){
+    for (int i = 1; i <= size; i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout <<"*";
+        }
+        std::cout<< "\n";
+    }
+}
+
+void DrawIsoslesTraingle(int size){
+    int space;
+
+    for (int i = 1,k = 0; i <=size ; ++i,k =0 ) {
+        for (space = 1; space <= size-i ; ++space) {
+            std::cout<<" ";
+        }
+        while (k!=2*i-1){
+            std::cout<<"*";
+            ++k;
+        }
+        std::cout<<std::endl;
+    }
+}
+int main() {
+    std::cout<<"Please Enter the number of rows\n"<<std::endl;
+    int size ;
+    std::cin>>size;
+//    DrawRightAngledTriangle(size);
+    DrawIsoslesTraingle(size);
+    return 0;
+}
+
