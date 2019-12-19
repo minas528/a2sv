5232e88ab780fe914dbcebdd28b27c7d1c091f2e
Minas <54796851+minas528@users.noreply.github.com>
2019-12-19 17:35:07 +0300

    updated with reccursion

--- Tribonacci Number.py
+++ Tribonacci Number.py
@@ -1,11 +1,9 @@
 class Solution:
+    def __init__(self):
+        self.dicts = {}
     def tribonacci(self, n: int) -> int:
-        tri = {}
-        for i in range(n+1):
-            if i == 0:
-                tri[i] = 0
-            elif i == 1 or i == 2:
-                tri[i] = 1
-            else:
-                tri[i] = tri[i-1] + tri[i-2] + tri[i-3]
-        return tri[n]
+        if n in self.dicts: return self.dicts[n]
+        if n == 0: self.dicts[0] = 0
+        elif n==1 or n ==2: self.dicts[1],self.dicts[2]= 1,1
+        else: self.dicts[n]= self.tribonacci(n-3)+self.tribonacci(n-2)+self.tribonacci(n-1)
+        return self.dicts[n]
