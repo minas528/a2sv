556cb7f307e90bcd967fcf149b4eca83450f45a9
Minas <54796851+minas528@users.noreply.github.com>
2020-01-13 12:10:29 +0300

    Update prefect Number.py

--- Introdution to Competitive Programming/D22/prefect Number.py
+++ Introdution to Competitive Programming/D22/prefect Number.py
@@ -2,13 +2,13 @@ class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         if num == 1 or num <= 0:
             return False
-        i, fout = 2, []
+        i, sums = 2, []
         while i <= math.sqrt(num):
             if num % i == 0:
-                fout.append(i)
-                fout.append(num/i)
+                sums.append(i)
+                sums.append(num/i)
             i += 1
-        return num == sum(fout)+1
+        return num == sum(sums)+1
 if __name__ == '__main__':
     sol = Solution()
     sol.checkPerfectNumber(28)
