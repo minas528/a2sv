af5e2a26af0f7f092c40fc60dfb38bf7937d51f6
Minas <54796851+minas528@users.noreply.github.com>
2020-01-17 16:36:29 +0300

    Update MezoPlayingZoma.py

--- Introdution to Competitive Programming/D23/MezoPlayingZoma.py
+++ Introdution to Competitive Programming/D23/MezoPlayingZoma.py
@@ -1,15 +1,13 @@
 def conTest(string):
         l_count,r_count = 0,0
-            for sstr in string:
-                        if sstr == 'L':
-                                        l_count +=1
-                                                elif sstr == 'R':
-                                                                r_count +=1
-                                                                    return l_count+r_count+1
+        for sstr in string:
+                if sstr == 'L':
+                        l_count +=1
+                elif sstr == 'R':
+                        r_count +=1
+        return l_count+r_count+1
 
-                                                                if __name__ == '__main__':
-                                                                        result =0
-                                                                            size = eval(input())
-                                                                                string = input()
-                                                                                    result = conTest(string)
-                                                                                        print(result)
+if __name__ == '__main__':
+        size = eval(input())
+        string = input()
+        print(conTest(string))
