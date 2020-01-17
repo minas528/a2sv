9b201fe8a6426360e2e7bd35e8a80603a88572bd
minas528 <minalem.5d@gmail.com>
2020-01-17 16:33:01 +0300

    create mezo playing zoma

---
+++ Introdution to Competitive Programming/D23/MezoPlayingZoma.py
@@ -0,0 +1,15 @@
+def conTest(string):
+        l_count,r_count = 0,0
+            for sstr in string:
+                        if sstr == 'L':
+                                        l_count +=1
+                                                elif sstr == 'R':
+                                                                r_count +=1
+                                                                    return l_count+r_count+1
+
+                                                                if __name__ == '__main__':
+                                                                        result =0
+                                                                            size = eval(input())
+                                                                                string = input()
+                                                                                    result = conTest(string)
+                                                                                        print(result)
