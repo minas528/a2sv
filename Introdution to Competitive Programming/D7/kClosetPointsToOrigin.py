7fd771618d608dc4474b3e418fb8852be39c829f
minas528 <minalem.5d@gmail.com>
2020-02-07 15:54:40 +0300

    k closest points to origint

---
+++ Introdution to Competitive Programming/D7/kClosetPointsToOrigin.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        return sorted(points, key=lambda e: e[0]**2+e[1]**2)[0:K]
