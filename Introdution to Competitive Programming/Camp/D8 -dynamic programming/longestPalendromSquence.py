99e76e0f7db662d7cc3599c001d623e41c5cee8b
minas528 <minalem.5d@gmail.com>
2020-02-14 02:02:29 +0300

    some dp

--- Introdution to Competitive Programming/Camp/D8 -dynamic programming/longestPalendromSquence.py
+++ Introdution to Competitive Programming/Camp/D8 -dynamic programming/longestPalendromSquence.py
@@ -1,19 +1,21 @@
-def longestPalindromeSubseq(self, s):
-        if s == s[::-1]:
-            return len(s)
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        return self.helper(s, 0, len(s) - 1, {})
 
-        n = len(s)
-        dp = [0 for j in xrange(n)]
-        dp[n-1] = 1
+    def helper(self, string, first, last, visited):
+        if (first, last) in visited:
+            return visited[(first, last)]
+        if first == last:
+            return 1
+        if first > last:
+            return 0
+        if string[first] == string[last]:
+            visited[(first, last)] = self.helper(string, first + 1, last - 1, visited) + 2
+            return visited[(first, last)]
+        visited[(first, last)] = max(self.helper(string, first + 1, last, visited),
+                                     self.helper(string, first, last - 1, visited))
+        return visited[(first, last)]
 
-        for i in xrange(n-1, -1, -1):   # can actually start with n-2...
-            newdp = dp[:]
-            newdp[i] = 1
-            for j in xrange(i+1, n):
-                if s[i] == s[j]:
-                    newdp[j] = 2 + dp[j-1]
-                else:
-                    newdp[j] = max(dp[j], newdp[j-1])
-            dp = newdp
-                    
-        return dp[n-1]
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.longestPalindromeSubseq("bbbab"))
--- Introdution to Competitive Programming/Camp/D8 -dynamic programming/numRolesToTarget.py
+++ Introdution to Competitive Programming/Camp/D8 -dynamic programming/numRolesToTarget.py
@@ -1,14 +1,24 @@
 class Solution:
+    def __init__(self):
+        self.memo = {}
     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
-        memo = {}
-        def dp(d, target):
-            if d == 0:
-                return 0 if target > 0 else 1
-            if (d, target) in memo:
-                return memo[(d, target)]
-            to_return = 0
-            for k in range(max(0, target-f), target):
-                to_return += dp(d-1, k)
-            memo[(d, target)] = to_return
-            return to_return
-        return dp(d, target) % (10**9 + 7)
+        if d == 0 and target == 0:
+            return 1
+        if d == 0 or target == 0 :
+            return 0
+        
+        cur = (d,target)
+        if cur in self.memo:
+            return self.memo[cur]
+        res = 0
+        for i in range(1,f+1):
+            if target >= i:
+                res += self.numRollsToTarget(d-1,f,target-i)
+            else: break
+        self.memo[cur] = res
+        return res%(1000000000+7)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.numRollsToTarget(2,6,7))
